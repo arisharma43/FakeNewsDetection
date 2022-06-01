2e293fce6a9b99739ab94d332bc3ff991a20a57a
arisharma43 <70657612+arisharma43@users.noreply.github.com>
2022-06-01 14:06:03 -0500

    Add visualization and tokenization

--- Classify.py
+++ Classify.py
@@ -1,5 +1,7 @@
+from lib2to3.pgen2 import token
 import nltk
 nltk.download('punkt')
+
 
 import tensorflow as tf
 import pandas as pd
@@ -8,6 +10,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import nltk
 import re
+import plotly.express as px
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
@@ -33,24 +36,24 @@ df_fake=pd.read_csv("Fake.csv")
 # print(df_fake.info())
 
 df_true['isFake']=0
-print(df_true.head())
+#print(df_true.head())
 
 df_fake['isFake']=1
-print(df_fake.head())
+#print(df_fake.head())
 
 df=pd.concat([df_true,df_fake]).reset_index(drop=True)
-print(df)
+#print(df)
 
 df.drop(columns=['date'],inplace=True)
 df['original']=df['title']+' '+df['text']
-print(df['original'][0])
+#print(df['original'][0])
 
 nltk.download("stopwords")
 
 from nltk.corpus import stopwords
 stop_words=stopwords.words('english')
 stop_words.extend(['from','subject','re','edu','use'])
-print(stop_words)
+#print(stop_words)
 
 def preprocess(text):
     result=[]
@@ -61,14 +64,37 @@ def preprocess(text):
     return result
 
 df['clean']=df['original'].apply(preprocess)
-print(df['original'][0])
-print(df['clean'][0])
+print(df['original'][2])
+print(df['clean'][2])
 
 #find total words
 list_of_words = []
 for i in df.clean:
     for j in i:
         list_of_words.append(j)
-print(list_of_words)
+# print(list_of_words)
 
 len(list_of_words)
+
+total_words=len(list(set(list_of_words)))
+# print(total_words)
+
+#join the words that are seperated by a space
+df['clean_joined']=df['clean'].apply(lambda x: " ".join(x))
+print(df['clean_joined'][2])
+
+plt.figure(figsize=(8,8))
+sns.countplot(y="subject",data=df)
+#sns.countplot(y="isFake",data=df)
+
+print(nltk.word_tokenize(df['clean_joined'][2]))
+
+maxlen=-1
+for doc in df.clean_joined:
+    tokens=nltk.word_tokenize(doc)
+    if(maxlen<len(tokens)):
+        maxlen=len(tokens)
+print("The max number of words in the doc is = ", maxlen)
+
+#fig=px.histogram(x=[len(nltk.word_tokenize(x)) for x in df.clean_joined],nbins=100)
+
